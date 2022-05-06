cd0963d9af39be6bb64bca11cad440ee28392205
Yossi <hayos@post.bgu.ac.il>
2022-05-06 16:24:11 +0300

    first commit

---
+++ flowers/flowers/flourist.cpp
@@ -0,0 +1,5 @@
+#include "flourist.h"
+#include <iostream>
+int main() {
+	std::cout << "hello";
+}
---
+++ flowers/flowers/flourist.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include <string>
+//using namespace std;
+class flourist
+{
+public:
+		int name;
+private:
+		std::string names;
+};
+
